1aa12a117d55ae2be042f77f614ae1003010c0b5
darlan <darlanfelisberto@gmail.com>
2022-10-16 20:41:58 -0300

    inicio auth

---
+++ frontEnd/academico/src/app/template/header.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector : 'header-component',
+  template: `
+    <header>
+      header
+    </header>
+  `
+})
+export class HeaderComponent{
+
+}
